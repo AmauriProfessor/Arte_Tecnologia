9d84b440979e4beb6e2464892fab48ba02384a8d
AmauriProfessor <143153228+AmauriProfessor@users.noreply.github.com>
2023-10-03 14:50:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function askQuestion() {
+    const childName = document.getElementById('childName').value;
+    const question = document.getElementById('question').value;
+    const answer = document.getElementById('answer');
+
+    // Consulta a API da Wikipedia
+    fetch(`https://pt.wikipedia.org/api/rest_v1/page/summary/${question}`)
+        .then(response => response.json())
+        .then(data => {
+            // Verifica se a página foi encontrada
+            if (data.title) {
+                answer.innerHTML = `Olá, ${childName}! Aqui está a resposta para sua pergunta:<br><strong>${data.title}</strong>: ${data.extract}`;
+            } else {
+                answer.innerHTML = `Desculpe, ${childName}, não consegui encontrar uma resposta para essa pergunta.`;
+            }
+        })
+        .catch(error => {
+            answer.innerHTML = `Desculpe, ${childName}, ocorreu um erro ao buscar a resposta.`;
+        });
+}
